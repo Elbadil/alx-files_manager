5ac2b190d86d7d1df027a758427554b9f4b07b92
Elbadil <elbaliadil@gmail.com>
2024-03-09 15:54:56 +0100

    Create a new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,7 +1,9 @@
 import dbClient from '../utils/db';
 
+const sha1 = require('sha1');
+
 class UsersController {
-  static async postNew (req, res) {
+  static async postNew(req, res) {
     const { email, password } = req.body;
     if (!email) {
       return res.status(400).json({ error: 'Missing email' });
@@ -9,8 +11,17 @@ class UsersController {
     if (!password) {
       return res.status(400).json({ error: 'Missing password' });
     }
-    const collection = dbClient.db(dbClient.database).collection('users');
-    const user = await collection.find({email: email});
+    const collection = dbClient.client.db(dbClient.database).collection('users');
+    const user = await collection.find({ email }).toArray();
+    if (user.length > 0) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+    const hashPwd = sha1(password);
+    const insertUser = await collection.insertOne(
+      { email, password: hashPwd },
+    );
+    const userID = insertUser.insertedId;
+    return res.status(201).json({ id: userID, email });
   }
 }
 
